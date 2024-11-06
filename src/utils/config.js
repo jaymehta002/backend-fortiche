4d95d9e81adbf8dc1c41688582d5df3aaae9165e
jaymehta002 <jaymehta002@gmail.com>
2024-11-06 21:46:42 +0530

    max-age

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,12 +2,12 @@ export const cookieOptions = {
   httpOnly: true,
   secure: true,
   sameSite: "none",
-  // maxAge: 60 * 60 * 1000, // 1 hour
-  expires: new Date(Date.now() + 60 * 60 * 1000),
+  maxAge: 60 * 60 * 1000, // 1 hour
+  // expires: new Date(Date.now() + 60 * 60 * 1000),
 };
 
 export const refreshCookieOptions = {
   ...cookieOptions,
-  // maxAge: 10 * 24 * 60 * 60 * 1000,
-  expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
+  maxAge: 10 * 24 * 60 * 60 * 1000,
+  // expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
 };
