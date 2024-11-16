74fa50f54bf4750bd83457207bcc6a360b7ff71c
jaymehta002 <jaymehta002@gmail.com>
2024-11-16 22:40:17 +0530

    google-end

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,6 +47,8 @@ export const googleCallback = asyncHandler(async (req, res, next) => {
         const tokenParams = new URLSearchParams({
           accessToken: tokens.accessToken,
           refreshToken: tokens.refreshToken,
+          redirectUrl: redirectUrl,
+          user: JSON.stringify(user),
         }).toString();
 
         return res.redirect(`${process.env.CLIENT_URL}/google?${tokenParams}`);
