16a9a45d61c5da1b33c4511128777f7bcdbb5831
jaymehta002 <jaymehta002@gmail.com>
2024-11-23 13:14:09 +0530

    route

--- src/shipping/shipping.route.js
+++ src/shipping/shipping.route.js
@@ -2,6 +2,9 @@ import { Router } from "express";
 import {
   createShipping,
   getUserShipping,
+  getShippingById,
+  updateShipping,
+  deleteShipping,
   updateShippingStatus,
 } from "./shipping.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
@@ -11,8 +14,11 @@ const shippingRouter = Router();
 shippingRouter.use(auth);
 
 // Routes
-shippingRouter.post("/create", createShipping);
-shippingRouter.get("/fetch", getUserShipping);
-shippingRouter.patch("/update-status/:id", updateShippingStatus);
+shippingRouter.post("/", createShipping);
+shippingRouter.get("/", getUserShipping);
+shippingRouter.get("/:id", getShippingById);
+shippingRouter.put("/:id", updateShipping);
+shippingRouter.delete("/:id", deleteShipping);
+shippingRouter.patch("/:id/status", updateShippingStatus);
 
 export default shippingRouter;
