b15440e1e44acf98fe35b17d890597a7ccef0346
2sandeep7 <ssandeep8696@gmail.com>
2024-11-28 00:46:23 +0530

    fix:Wishlist Controller

--- src/whishlist/wishlist.controller.js
+++ src/whishlist/wishlist.controller.js
@@ -3,6 +3,7 @@ import { accountType } from "../common/common_constants.js";
 import { Product } from "../product/product.model.js";
 import { User } from "../user/user.model.js";
 import { ApiResponse } from "../utils/APIResponse.js";
+ 
 
 const addToWishListController = async (req, res, next) => {
   try {
@@ -10,16 +11,12 @@ const addToWishListController = async (req, res, next) => {
     const user = req.user;
 
     if (user.accountType !== accountType.INFLUENCER) {
-      throw new ApiError(403, "Unauthorized: Only influencers can add to wishlist");
+      return res.status(403).json("Unauthorized: Only influencers can add to wishlist");
     }
-
-    if (!mongoose.Types.ObjectId.isValid(productId)) {
-      throw new ApiError(400, "Invalid product ID");
-    }
-
+ 
     const product = await Product.findById(productId);
     if (!product) {
-      throw new ApiError(404, "Product not found");
+    return res.status(404).json("Product not found");
     }
 
     if (!Array.isArray(user.wishlist)) {
@@ -56,7 +53,7 @@ const getWishListController = async (req, res, next) => {
 
     const user = await User.findById(userId).populate('wishlist');
     if (!user) {
-      return res.status(404).json(new ApiResponse(404, null, "User not found"));
+      return res.status(404).json( "User not found");
     }
  
     console.log(user);
@@ -75,19 +72,16 @@ const removeFromWishListController = async (req, res, next) => {
     const user = req.user;
 
     if (user.accountType !== accountType.INFLUENCER) {
-      throw new ApiError(403, "Unauthorized: Only influencers can modify wishlist");
+      return res.status(403).json( "Unauthorized: Only influencers can modify wishlist");
     }
  
-    if (!mongoose.Types.ObjectId.isValid(productId)) {
-      throw new ApiError(400, "Invalid product ID");
-    }
  
     const isProductInWishlist = user.wishlist.some(
       item => item.toString() === productId
     );
 
     if (!isProductInWishlist) {
-      throw new ApiError(404, "Product not found in wishlist");
+      return res.status(404).json( "Product not found in wishlist");
     }
 
     const updatedUser = await User.findByIdAndUpdate(
