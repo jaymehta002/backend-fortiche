ce80471e0d482eee8f31b5e0b17009f9ac6ea448
Jay Mehta <jaymehta002@gmail.com>
2025-01-20 21:10:07 +0530

    build

---
+++ public/dist/assets/Explore-Template-BMUQ-6-L.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-17S3Hvul.js";import{T as r}from"./TemplateCard-CKIlNxGT.js";const o=()=>e.jsx("div",{className:"flex flex-col gap-4 rounded-lg",children:e.jsx(r,{})});export{o as default};
